e40426aa3722521ac769ef1821501ca92e106c58
Lawrence Toomey <too043@csiro.au>
2025-03-25 12:36:04 +1100

    Replace 'sdhdfProc' with 'INSPECTA' in license

--- inspecta/src/sdhdf_modify.c
+++ inspecta/src/sdhdf_modify.c
@@ -9,18 +9,18 @@
 //  Copyright (C) 2019, 2020, 2021, 2022 George Hobbs
 
 /*
- *    This file is part of sdhdfProc. 
+ *    This file is part of INSPECTA. 
  * 
- *    sdhdfProc is free software: you can redistribute it and/or modify 
+ *    INSPECTA is free software: you can redistribute it and/or modify 
  *    it under the terms of the GNU General Public License as published by 
  *    the Free Software Foundation, either version 3 of the License, or 
  *    (at your option) any later version. 
- *    sdhdfProc is distributed in the hope that it will be useful, 
+ *    INSPECTA is distributed in the hope that it will be useful, 
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of 
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
  *    GNU General Public License for more details. 
  *    You should have received a copy of the GNU General Public License 
- *    along with sdhdfProc.  If not, see <http://www.gnu.org/licenses/>. 
+ *    along with INSPECTA.  If not, see <http://www.gnu.org/licenses/>. 
  */
 
 //
